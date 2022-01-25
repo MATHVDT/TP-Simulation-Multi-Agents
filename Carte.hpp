111ab922335a4758e4615b126346e2f091f8e509
nadesnos <nathan.desnos@etu.uca.fr>
2022-01-25 10:57:09 +0100

    amélioration de l'affichage de la carte

--- Carte.hpp
+++ Carte.hpp
@@ -1,16 +1,21 @@
 #ifndef CARTE_HPP
 #define CARTE_HPP
 
-const int taille = 10;
+class Agent{};
+
+const int TAILLE = 10;
 
 class Carte
 {
-    int _grille[taille][taille];
+    int _grille[TAILLE][TAILLE];
+    Agent * _grilleAgents[TAILLE][TAILLE];
 
     public:
         Carte();
-        void AfficherCarte();
+        void AfficherCarte() const;
         void ChangerCase(int i, int j, int val);
+        Agent * getAgent(int i, int j);
+        void setAgent(int i, int j, Agent * agent);
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -9,8 +9,15 @@ int main()
 
     carte.AfficherCarte();
     cout << "-----" << endl;
-    carte.ChangerCase(3, 6, 10);
+    carte.ChangerCase(3, 6, 1);
     carte.ChangerCase(7, 7, 7);
+    for (int i  = 3; i < 7; i++)
+    {
+        for (int j = 2; j < 5; j++)
+        {
+            carte.ChangerCase(i, j, 2);
+        }
+    }
     carte.AfficherCarte();
 
     return 0;
